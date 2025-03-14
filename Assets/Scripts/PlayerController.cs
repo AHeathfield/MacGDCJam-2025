097e5c95352045796b8e3f13643ce90e87e05577
Aidan Heathfield <aidan.heathfield@gmail.com>
2025-03-13 21:07:10 -0400

    Feature #3: Added the switching between the time zones (scenes)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Scripting.APIUpdating;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -19,9 +21,26 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float dashDuration = 0.2f;
     [SerializeField] private float dashCooldown = 1.5f;
 
+    [Header("TimeChange Settings")]
+    [SerializeField] private float timeChangeDuration = 0.2f;
+    [SerializeField] private float timeChangeCooldown = 3.0f;
+
+    // Globals
+    private static Vector3 currentPos = new Vector3(0.0f, 0.0f, 0.0f);
+    private static Quaternion currentRot = new Quaternion();
+
+
     private bool _canDash;
     private bool _isDashing;
     private bool _dashInput;
+    
+    private bool _canTimeChange;
+    private bool _isTimeChanging;
+
+    private bool _timeInput;
+    private bool _presentInput;
+    private bool _pastInput;
+    private bool _futureInput;
     
     private InputSystem_Actions _playerInputActions;
     private Vector3 _input;
@@ -33,6 +52,7 @@ public class PlayerController : MonoBehaviour
         _playerInputActions = new InputSystem_Actions();
 
         _canDash = true;
+        _canTimeChange = true;
     }
 
     void OnEnable()
@@ -43,6 +63,12 @@ public class PlayerController : MonoBehaviour
     void OnDisable()
     {
         _playerInputActions.Player.Disable();
+    }
+
+    void Start()
+    {
+        this.transform.position = PlayerController.currentPos;
+        this.transform.rotation = PlayerController.currentRot;
     }
 
     void Update()
@@ -63,10 +89,19 @@ public class PlayerController : MonoBehaviour
         Look();
         CalculateSpeed();
         Move();
+        TimeChange();
 
+        // Dash routine
         if (_dashInput && _canDash)
         {
             StartCoroutine(Dash());
+        }
+
+        // Time Change routine
+        if (_canTimeChange && _timeInput)
+        {
+            _canTimeChange = false;
+            StartCoroutine(TimeSwitch());
         }
     }
 
@@ -76,8 +111,21 @@ public class PlayerController : MonoBehaviour
         Vector2 movementInput = _playerInputActions.Player.Move.ReadValue<Vector2>();
         _input = new Vector3(movementInput.x, 0, movementInput.y);
         _dashInput = _playerInputActions.Player.Sprint.IsPressed();
+        
+        if (_canTimeChange)
+        {
+            _presentInput = _playerInputActions.Player.Present.IsPressed();
+            _pastInput = _playerInputActions.Player.Past.IsPressed();
+            _futureInput = _playerInputActions.Player.Future.IsPressed();
+        }
+        
+        if (_presentInput || _pastInput || _futureInput)
+        {
+            _timeInput = true;
+        }
+        
 
-        Debug.Log(_input);
+        // Debug.Log(_timeInput);
     }
 
     void Look()
@@ -122,6 +170,26 @@ public class PlayerController : MonoBehaviour
         
     }
 
+    // Handles the time change
+    void TimeChange()
+    {
+        if (_isTimeChanging)
+        {
+            if (_presentInput)
+            {
+                SceneManager.LoadScene("Present");
+            }
+            else if (_pastInput)
+            {
+                SceneManager.LoadScene("Past");
+            }
+            else if (_futureInput)
+            {
+                SceneManager.LoadScene("Future");
+            }
+        }
+    }
+
     IEnumerator Dash()
     {
         _canDash = false;
@@ -130,5 +198,23 @@ public class PlayerController : MonoBehaviour
         _isDashing = false;
         yield return new WaitForSeconds(dashCooldown);
         _canDash = true;
+    }
+
+    // The coroutine for the time change
+    IEnumerator TimeSwitch()
+    {
+        // So we don't spawn at the spawn point of the scene we keep our current position
+        PlayerController.currentPos = transform.position;
+        PlayerController.currentRot = transform.rotation;
+
+        _canTimeChange = false;
+        _isTimeChanging = true;
+        yield return new WaitForSeconds(timeChangeDuration);
+        _isTimeChanging = false;
+        yield return new WaitForSeconds(timeChangeCooldown);
+        _canTimeChange = true;
+
+        // Putting us back to the position before we switched time
+        // transform.position = currentPos; 
     }
 }
